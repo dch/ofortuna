d015c03206e497cc592bb255b2539392db27305d
John Arundel <john@bitfieldconsulting.com>
2021-04-27 13:02:06 +0100

    TestGetFortuneReturnOK passes

--- app/app.go
+++ app/app.go
@@ -4,14 +4,20 @@ import (
 	"fmt"
 	"net/http"
 	"ofortuna"
+	"time"
 )
 
 func ListenAndServe(address string) error {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		w.WriteHeader(http.StatusTeapot)
-		fmt.Fprintf(w, ofortuna.GetRandomFortune())
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintln(w, ofortuna.GetRandomFortune())
 	})
-
-	return http.ListenAndServe(address, nil)
+	s := &http.Server{
+		Addr: address,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,			    Handler: mux,
+	}
+	return s.ListenAndServe()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module ofortuna
 
 go 1.16
+
+require github.com/phayes/freeport v0.0.0-20180830031419-95f893ade6f2
